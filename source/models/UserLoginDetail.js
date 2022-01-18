27f5765d2aea17114b67fdc5872edbfb6fb650ea
Marco Monti <monti.marco92@hotmail.com>
2022-01-18 12:14:32 -0300

    UserLoginDetail model fixed and index.js deleted from models folder

--- source/models/UserLoginDetail.js
+++ source/models/UserLoginDetail.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-	sequelize.define("password", {
+	sequelize.define("userLoginDetail", {
 		password: {
 			type: DataTypes.STRING(30),
 		},
