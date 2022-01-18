b0b9042cc2a99ea77b47310db54bb57f08d68eea
Marco Monti <monti.marco92@hotmail.com>
2022-01-18 18:14:18 -0300

    cors, body-parser, cookie-parser and morgan dependecies installed, app.js in progress

--- source/app.js
+++ source/app.js
@@ -1,1 +1,24 @@
-console.log("prueba");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
+
+require("./db");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Front-End url
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
