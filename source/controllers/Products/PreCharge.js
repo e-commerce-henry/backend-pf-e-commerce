5e450acc353eb441e2738bd3e1e5e261c03a9376
Cristian Silva <81317103+silcoro@users.noreply.github.com>
2022-01-20 18:25:35 -0500

    Develop cris (#23)
    * edit delete product category getproducById y Precharge product y categories
    
    * edit precharge
    
    * get product y get product x name

--- source/controllers/Products/PreCharge.js
+++ source/controllers/Products/PreCharge.js
@@ -2,14 +2,15 @@ const BD  =  require('../../../BD.json')
 const { Category, Product } = require('../../db');
 
 const preChargeCategories= async (req, res, next) =>{
-
     try {
+        //obtengo el nombre de la categorias
         let filtro_cat = await BD.map(e =>e.Category);
+        // filtro las categorias sin undefined
         let list_cate =await filtro_cat.filter( e => e!== undefined)
+        //me crea un nuevo array sin palabras repetidas
         let cat = [...new Set(list_cate)]
-
+        //me crea las categorias
         await cat.map( p => Category.findOrCreate({where:{name:p}}))
-        
     } catch (error) {
         next(error)
     }
@@ -21,9 +22,9 @@ const preChargeProduct = async (req, res, next) =>{
     let filter_BD = await BD.filter(e => e.Category !== undefined);
 //aqui se ejecutara y creara por cada objeto del array filtrado
     for (const i of filter_BD) {
+        //busco la categoria y la almaceno en cat_id
         let cat_id =await Category.findOne({where:{name:i.Category}})
-        // console.log('cat_id :>> ', cat_id);
-
+        // me crea el producto
         await Product.findOrCreate({
             where:{name:i.Name},
             defaults:{
@@ -32,11 +33,10 @@ const preChargeProduct = async (req, res, next) =>{
                 img: i.Img,
                 brand: i.Brand,
                 description: i.Description,
-                categoryId: cat_id.id
+                categoryId: cat_id.id//asigno el id
             }
         })
     }
-
     } catch (error) {
         next(error)
     }
