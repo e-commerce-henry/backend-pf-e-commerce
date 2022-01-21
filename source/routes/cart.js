84a3e9863df7fbee1e50267ed8f86bb94b785c0b
Marco Monti <monti.marco92@hotmail.com>
2022-01-21 12:43:37 -0300

    delete Cart Item Route created

--- source/routes/cart.js
+++ source/routes/cart.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const postCartItem = require("../controllers/Cart/postCartItem");
+const deleteCartItem = require("../controllers/Cart/delelteCartItem");
 
 router.post("/", postCartItem);
+router.delete("/", deleteCartItem);
 
 module.exports = router;
