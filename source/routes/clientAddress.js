acdcfb34bc91c93171efb17305bdd7786c8abaa7
Marco Monti <monti.marco92@hotmail.com>
2022-01-23 15:10:39 -0300

    editClientAddress route created

--- source/routes/clientAddress.js
+++ source/routes/clientAddress.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const addClientAddress = require("../controllers/ClientAddress/postClientAddress");
+const editClientAddress = require("../controllers/ClientAddress/editClientAddress");
 
 router.post("/:id", addClientAddress);
+router.put("/:id/:addressId", editClientAddress);
 
 module.exports = router;
