d1815766c46f23e9e1494c108f0e70df480369f7
Marco Monti <monti.marco92@hotmail.com>
2022-01-25 20:49:00 -0300

    sale banner routes modified to set Salebanner panel in admin controls

--- source/models/SaleBanner.js
+++ source/models/SaleBanner.js
@@ -2,6 +2,10 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
 	sequelize.define("saleBanner", {
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
 		discount: {
 			type: DataTypes.FLOAT,
 			allowNull: false,
