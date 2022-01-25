6c65ed4d7ff3abe6eb40d07cccda471d28bb6d75
Marco Monti <monti.marco92@hotmail.com>
2022-01-25 12:06:21 -0300

    getAllUsers route created

--- source/routes/users.js
+++ source/routes/users.js
@@ -3,9 +3,11 @@ const postUser = require("../controllers/Users/postUser");
 const deleteUser = require("../controllers/Users/deleteUser");
 const editUser = require("../controllers/Users/editUser");
 const { requireAuth, isAdmin } = require("../middleware/authMiddleware");
+const getAllUsers = require("../controllers/Users/getUsers");
 
 router.post("/", postUser);
-router.delete("/:id", [requireAuth, isAdmin], deleteUser);
+router.delete("/:id", deleteUser);
 router.put("/:id", editUser);
+router.get("/", getAllUsers); // should be protected
 
 module.exports = router;
