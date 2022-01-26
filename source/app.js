ca4d793c5354aa793dccb8194dfaf435570c86ad
Marco Monti <monti.marco92@hotmail.com>
2022-01-26 16:01:09 -0300

    se prueba condicional en app.js para poder admitir mas de un puerto y no tener conflicto con cookies

--- source/app.js
+++ source/app.js
@@ -3,23 +3,36 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+const cors = require("cors");
 
 require("./db");
 
 const app = express();
-
+app.use(
+	cors({
+		credentials: true,
+		origin: true,
+	})
+);
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*"); // Front-End all url "*"
-	res.header("Access-Control-Allow-Credentials", "true");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	const corsWhitelist = ["http://localhost:3000", "http://localhost:4000"];
+	if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+		res.header("Access-Control-Allow-Origin", req.headers.origin); // Front-End all url "*"
+		res.header("Access-Control-Allow-Credentials", "true");
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
+		res.header(
+			"Access-Control-Allow-Methods",
+			"GET, POST, OPTIONS, PUT, DELETE"
+		);
+	}
+
 	next();
 });
 
