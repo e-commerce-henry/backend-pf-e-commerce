35b64f0e0a836431d6a0d2eb68b094bdde4931bb
Marco Monti <monti.marco92@hotmail.com>
2022-01-30 12:20:47 -0300

    ruta para traer todas las ordenes de un usuario por id modificada

--- source/routes/order.js
+++ source/routes/order.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const editOrder = require("../controllers/Order/editOrder");
 const getAllOrders = require("../controllers/Order/getAllOrders");
-const getHistoryById = require("../controllers/Order/getHistoryUser");
+const getAllOrdersByUser = require("../controllers/Order/getAllOrdersByUser");
 const createOrder = require("../controllers/Order/postOrder");
 
-router.get("/:idUser", getHistoryById);
+router.get("/:userId", getAllOrdersByUser);
 router.get("/", getAllOrders);
 router.post("/:userId", createOrder);
 router.put("/:userId", editOrder);
