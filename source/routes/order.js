1fff6318038838c37b119555c839c8a72d06d701
Marco Monti <monti.marco92@hotmail.com>
2022-01-30 12:01:37 -0300

    getAllOrders route created

--- source/routes/order.js
+++ source/routes/order.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const editOrder = require("../controllers/Order/editOrder");
+const getAllOrders = require("../controllers/Order/getAllOrders");
 const getHistoryById = require("../controllers/Order/getHistoryUser");
 const createOrder = require("../controllers/Order/postOrder");
 
 router.get("/:idUser", getHistoryById);
+router.get("/", getAllOrders);
 router.post("/:userId", createOrder);
 router.put("/:userId", editOrder);
 
