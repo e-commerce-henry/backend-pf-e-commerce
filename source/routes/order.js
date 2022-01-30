2a3597cd9a0615e16826b2917192531e6b489289
Marco Monti <monti.marco92@hotmail.com>
2022-01-30 11:41:09 -0300

    editOrder route was created to modify order status depending on response from ML and shippingStatus

--- source/routes/order.js
+++ source/routes/order.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
+const editOrder = require("../controllers/Order/editOrder");
 const getHistoryById = require("../controllers/Order/getHistoryUser");
 const createOrder = require("../controllers/Order/postOrder");
 
 router.get("/:idUser", getHistoryById);
 router.post("/:userId", createOrder);
+router.put("/:userId", editOrder);
 
 module.exports = router;
