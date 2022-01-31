cfb7efb47ccc13e9bec5c5c58c3bf0caac03f900
Marco Monti <monti.marco92@hotmail.com>
2022-01-31 19:15:13 -0300

    erased domain on cookie

--- source/controllers/Auth/signIn.js
+++ source/controllers/Auth/signIn.js
@@ -35,19 +35,11 @@ const signIn = async (req, res) => {
 						sameSite: "none",
 						secure: true,
 						maxAge: maxAge * 1000,
-
-
-						domain: "boring-dubinsky-207e29.netlify.app",
-
 					});
 					res.cookie("jwt-Logged", process.env.CLIENT_SIDE_AUTH_COOKIE, {
 						maxAge: maxAge * 1000,
 						sameSite: "none",
 						secure: true,
-
-
-						domain: "boring-dubinsky-207e29.netlify.app",
-
 					});
 					res.status(200).send({ user: foundUser.id });
 				} else {
