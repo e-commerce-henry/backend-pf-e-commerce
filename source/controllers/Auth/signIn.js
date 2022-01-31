e655986f0b23412808c1978f6a600eeb80c7f070
Marco Monti <88577526+MontiMarco92@users.noreply.github.com>
2022-01-31 18:44:57 -0300

    Fix cookie config (#64)
    * changed cookie config for deploy
    
    * jwt-logged cookie config changed
    
    * netlify domain added
    
    * continue fixing cookies
    
    * fix2
    
    * fix 4
    
    * signIn route modified
    
    * fixing cookies

--- source/controllers/Auth/signIn.js
+++ source/controllers/Auth/signIn.js
@@ -35,13 +35,17 @@ const signIn = async (req, res) => {
 						sameSite: "none",
 						secure: true,
 						maxAge: maxAge * 1000,
-						domain: "herokuapp.com",
+
+						domain: "admin-pf-e-commerce.herokuapp.com",
+
 					});
 					res.cookie("jwt-Logged", process.env.CLIENT_SIDE_AUTH_COOKIE, {
 						maxAge: maxAge * 1000,
 						sameSite: "none",
 						secure: true,
-						domain: "herokuapp.com",
+
+						domain: "admin-pf-e-commerce.herokuapp.com",
+
 					});
 					res.status(200).send({ user: foundUser.id });
 				} else {
