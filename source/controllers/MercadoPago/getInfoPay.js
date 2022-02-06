71e7c7261f5251852318f4052d312181a601337e
Marco Monti <monti.marco92@hotmail.com>
2022-02-06 17:10:02 -0300

    se modifica ruta getInfoPay para que luego de efectuada compra exitosa se actualice el stock de cada producto comprado en la db

--- source/controllers/MercadoPago/getInfoPay.js
+++ source/controllers/MercadoPago/getInfoPay.js
@@ -1,22 +1,33 @@
 const e = require("express");
-const { Order } = require("../../db");
+const { Order, OrderDetail, Product } = require("../../db");
 
 const getInfoPay = async (req, res) => {
 	try {
 		const { status, external_reference } = req.query;
 
-		const order = await Order.findByPk(external_reference);
+		const order = await Order.findByPk(external_reference, {
+			include: { model: OrderDetail },
+		});
 		console.log(order);
 		const newOrderStatus = status === "approved" ? "completed" : "cancelled";
 
 		await order.update({ status: newOrderStatus });
 		console.log("orden actualizada");
 
+		//si es aprobada, actualizo el stock en db
+		if (status === "approved") {
+			const promises = order.orderDetails.map(async (e) => {
+				return await Product.decrement("stock", {
+					by: e.quantity,
+					where: { id: e.productId },
+				});
+			});
+			return await Promise.all(promises);
+		}
+
 		newOrderStatus === "completed"
 			? res.redirect("http://localhost:3000/realizado")
 			: res.redirect("http://localhost:3000/rechazada"); // crear componente y rutearlo
-
-		// res.send("todo liso");
 	} catch (err) {
 		res.status(500).send(err);
 	}
