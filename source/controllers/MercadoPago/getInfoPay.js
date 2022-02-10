8f37e27ab41f9229c73cb065f7ffe42d3572f77f
Marco Monti <monti.marco92@hotmail.com>
2022-02-10 11:19:52 -0300

    cambio ruta a heroku

--- source/controllers/MercadoPago/getInfoPay.js
+++ source/controllers/MercadoPago/getInfoPay.js
@@ -49,8 +49,8 @@ const getInfoPay = async (req, res) => {
 		}
 
 		newOrderStatus === "completed"
-			? res.redirect("https://nifty-archimedes-2faaf7.netlify.app/realizado")
-			: res.redirect("https://nifty-archimedes-2faaf7.netlify.app/rechazada");
+			? res.redirect("http://cliente-pf-e-commerce.herokuapp.com/realizado")
+			: res.redirect("http://cliente-pf-e-commerce.herokuapp.com/rechazada");
 	} catch (err) {
 		res.status(500).send(err);
 	}
